65faaad6530b15b079a679d79cfe6f2193f775f7
hursitti <harrisonursitti@gmail.com>
2025-03-25 15:55:00 -0400

    Updated problem 5

--- problem5.py
+++ problem5.py
@@ -2,7 +2,7 @@ maxPrimeFactors = {}
 
 def populatePrimeFactors(maxNum):
     x = 2
-    while x < maxNum:
+    while x < maxNum + 1:
         prime = True
         for key in maxPrimeFactors.keys():
             if (x % key == 0):
@@ -12,7 +12,23 @@ def populatePrimeFactors(maxNum):
             maxPrimeFactors[x] = 0
         x += 1
 
-populatePrimeFactors(20)
+def numberPrimeFactors(num):
+    factors = {}
+    temp = num
+    for x in maxPrimeFactors:
+        while (temp % x == 0):
+            if (factors.get(x) == None):
+                factors[x] = 1
+            else:
+                factors[x] += 1
+            temp /= x
+    return factors
 
-print (maxPrimeFactors)
+def uniqueSums(low, top):
+    populatePrimeFactors(top)
+    
+
+
+
+
 
