2ffcc823680150b8070e4e8caaa97c0474872cb5
hursitti <harrisonursitti@gmail.com>
2025-03-27 08:48:09 -0400

    First solution for problem 7
    I could limit the search by only checking primes upto the square root of the number

---
+++ problem7.py
@@ -0,0 +1,19 @@
+primeList = []
+
+def isPrime(x):
+    for y in primeList:
+        if (x % y == 0):
+            return False
+    return True
+
+def populateUptoPrime(num):
+    i = 0
+    x = 2
+    while i < num:
+        if (isPrime(x)):
+            i += 1
+            primeList.append(x)
+        x += 1
+        
+populateUptoPrime(10001)
+print(primeList[10000])
