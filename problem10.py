48d05b8841e0c86e988737cd2841430d96e4c907
hursitti <harrisonursitti@gmail.com>
2025-04-07 08:47:34 -0400

    Completed problem 10
    completed problem 10 with a solution to generalize the 6k +- 1 rule for primes.

--- problem10.py
+++ problem10.py
@@ -1,3 +1,6 @@
+import time
+import sys
+
 def primeMultiplier (primes):
     reduced = 1
     unreduced = 0
@@ -38,19 +41,18 @@ def primishes (primes):
     ret = []
     for x in primes:
         product *= x
-    one = product - 1
-    i = product
+    i = product - 1
     while i > 0:
         primish = True
         for x in primes:
             if (i % x == 0):
                 primish = False
-        if (primish and i != one):
+        if (primish):
             ret.append(i)
         i -= 1
     return (product, ret)
 
-print (primishes([2, 3, 5]))
+#print (primishes([2, 3, 5, 7]))
 
 def prime(primes, limit, x):
     if x > limit:
@@ -67,21 +69,48 @@ def sumOfPrimes(basePrimes, upto):
     subs = p[1]
     i = step
     end = upto + step
+    total = long(0)
+    total += sum(basePrimes)
     while i < end:
         for x in subs:
             possiblePrime = i - x
-            if prime(primes, upto, possiblePrime):
-                primes.append(possiblePrime)
+            if (possiblePrime != 1):
+                if prime(primes, upto, possiblePrime):
+                    primes.append(possiblePrime)
+                    total += possiblePrime
         i += step
-    print primes
-    print (sum(primes) + sum(basePrimes))
+    return total
 
-sumOfPrimes([2, 3, 5, 7, 11], 1000)
+def runTrial(bp, upto):
+    start = time.time()
+    sop = sumOfPrimes(bp, upto)
+    end = time.time()
+    diff = end - start
+    return (start, end, sop, diff)
+
+def compareTrial(bp1, bp2, upto, trials):
+    meanDiff1 = float(0)
+    meanDiff2 = float(0)
+    for x in range(0, trials):
+        ret1 = runTrial(bp1, upto)
+        ret2 = runTrial(bp2, upto)
+
+        meanDiff1 += ret1[3]
+        meanDiff2 += ret2[3]
+
+        print bp1
+        print ret1
+        print bp2
+        print ret2
+
+    meanDiff1 /= trials
+    meanDiff2 /= trials
+
+    print("\nMean Difference for first base prime list: " + str(meanDiff1))
+    print("Mean Difference for second base prime list: " + str(meanDiff2))
 
 
-
-
-
+compareTrial([2], [2, 3, 5, 7, 11, 13], 2000000, 1)
 
 
 
