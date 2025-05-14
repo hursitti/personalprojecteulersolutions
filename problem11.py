d3fba08a289b32d88fb60485ce779316ca3894e1
hursitti <harrisonursitti@gmail.com>
2025-05-14 14:46:42 -0400

    Temp solution
    This found the solution but needs edits for correctness and organization.

--- problem11.py
+++ problem11.py
@@ -22,12 +22,12 @@ raw = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08\n\
 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"
 
 rawTemp = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08\n\
-49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00\n\
-81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65\n\
-52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91\n\
-22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80\n\
-24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50\n\
-32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70\n\
+49 49 99 40 17 99 18 57 60 87 17 40 98 43 69 48 04 56 62 00\n\
+81 49 31 73 99 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65\n\
+52 70 95 99 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91\n\
+22 31 99 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80\n\
+24 99 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50\n\
+99 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70\n\
 67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21\n\
 24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72\n\
 21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95\n\
@@ -36,11 +36,11 @@ rawTemp = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08\n\
 86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58\n\
 19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40\n\
 04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66\n\
-88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 99\n\
-04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 99 99\n\
-20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 99 99 16\n\
-20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 99 99 05 54\n\
-01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 99 99 19 67 48"
+88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69\n\
+04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 99 62 76 36\n\
+20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 0 74 04 36 16\n\
+20 73 35 29 78 31 90 01 74 31 49 71 48 86 99 16 23 57 05 54\n\
+01 70 54 71 83 51 54 69 16 92 33 48 61 99 52 01 89 19 67 48"
 
 def parseData(rawData):
     ret = []
@@ -53,7 +53,7 @@ def parseData(rawData):
             ret.append(int(item))
     return (ret, height, width)
 
-parsed, height, width = parseData(rawTemp)
+parsed, height, width = parseData(raw)
 
 class Counter:
     def __init__(self, firstValues):
@@ -161,8 +161,8 @@ def swPass(data, height, width, answerLength):
     indexy = -1
 
     for column in range(answerLength - 1, width):
-        counter = Counter([getFromIndex(data, answerLength - (1 + t), 0 + t, width) for t in range(answerLength)])
-        m, indexx, indexy = check(counter, m, answerLength - 1, 0, indexx, indexy)
+        counter = Counter([getFromIndex(data, column - (1 + t), 0 + t, width) for t in range(answerLength)])
+        m, indexx, indexy = check(counter, m, column, 0, indexx, indexy)
         
         x, y = column - answerLength, answerLength
         while y < height and x > -1:
@@ -171,11 +171,11 @@ def swPass(data, height, width, answerLength):
             x -= 1
             y += 1
 
-    for row in range (1, height):
-        counter = Counter([getFromIndex(data, width - (1 + t), 1 + t, width) for t in range(answerLength)])
-        m, indexx, indexy = check(counter, m, width + 1 - answerLength, 0, indexx, indexy)
+    for row in range (1, height - answerLength):
+        counter = Counter([getFromIndex(data, width - (1 + t), row + t, width) for t in range(answerLength)])
+        m, indexx, indexy = check(counter, m, width - 1, row, indexx, indexy)
         
-        x, y = width - answerLength, answerLength
+        x, y = width - answerLength, row + answerLength
         while y < height and x > -1:
             counter.newValue(getFromIndex (data, x, y, width))
             m, indexx, indexy = check(counter, m, x + answerLength - 1, y + 1 - answerLength, indexx, indexy)
